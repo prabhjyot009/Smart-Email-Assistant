3ee446a2b613d937094320b0b790d4dc654d625d
prabhjyot009 <prabhjyot009s@gmail.com>
2025-02-10 17:22:05 +0530

    Initialize React project with Email Reply Generator app
    Set up a new React app with essential features, including email tone selection, API integration, and error handling. Added MUI for UI components, ESLint config for code linting, and appropriate `gitignore` entries for the project. Included Vite for development and build setup.

--- email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -2,14 +2,12 @@ package com.email.writer.app;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/email")
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
         private final EmailGeneratorService emailGeneratorService;
         @PostMapping("/generate")
